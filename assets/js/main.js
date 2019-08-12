4c8a91b82929f8e9e0f34b40011b62cd05a821c3
Jordy Lopez <jordizeus93@gmail.com>
2019-08-12 16:00:38 +0100

    Index.html
    Almost done, social network should be added and form should be validated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,27 @@
+//--------- Setters ----------
 setAlertPositionX('text');
 setAlertPositionX('icon');
 
+
+//---------- Getters and Methods. --------------
+
+/*Method to get the screen resolution.
+*@params param It will get the width or heigth of the current screen.
+*/
+function getScreen(param) {
+
+    if (param == 'width') {
+        return innerWidth;
+    } else if (param == 'heigth') {
+        return innerHeight;
+    } else {
+        console.log('Invalid parameter');
+    }
+}
+
+/**
+ * This function will set the discover more element to the center of the page.
+ */
 function setAlertPositionX(item) {
 
     //We obtain the width and heigth of the current screen.
@@ -12,16 +33,16 @@ function setAlertPositionX(item) {
      */
     if (item == 'text') {
 
-        var width = (widthScreen / 2) - 60 + "px";
-        var heigth = (heigthScreen - 50) + "px";
+        var width = (getScreen('width') / 2) - 60 + "px";
+        var heigth = (getScreen('heigth') - 50) + "px";
 
         document.getElementById('discover').style.top = heigth;
         document.getElementById('discover').style.left = width;
 
     } else if (item == "icon") {
 
-        var width = (widthScreen / 2) - 10 + "px";
-        var heigth = (heigthScreen - 30) + "px";
+        var width = (getScreen('width') / 2) - 10 + "px";
+        var heigth = (getScreen('heigth') - 30) + "px";
 
         document.getElementById('expand-icon').style.top = heigth;
         document.getElementById('expand-icon').style.left = width;
@@ -29,6 +50,9 @@ function setAlertPositionX(item) {
     }
 }
 
+/**
+ * This method will update the element when the screen is being resizable.
+ */
 function screenResize() {
     setAlertPositionX('text');
     setAlertPositionX('icon');
